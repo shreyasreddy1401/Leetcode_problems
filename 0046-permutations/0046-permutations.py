4309fc4ab9bded37c93db9cb97df3df2cd1082f6
shreyasreddy1401 <138093235+shreyasreddy1401@users.noreply.github.com>
2024-11-05 13:31:49 +0530

    Time: 0 ms (100%), Space: 17 MB (5.85%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,16 +1,15 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         ds = []
-        def K(nums,d,ds):
-            if len(d) == len(nums):
-                ds.append(d[:])
+        def K(nums,start,d,ds):
+            if start == len(nums):
+                ds.append(nums[:])
                 return
-            for i in range(len(nums)):
-                if nums[i] in d:
-                    continue
-                d.append(nums[i])
-                K(nums,d,ds)
-                d.pop()
-        K(nums,[],ds)
+            for i in range(start,len(nums)):
+                nums[i],nums[start] = nums[start],nums[i]
+                K(nums,start+1,d,ds)
+                nums[i],nums[start] = nums[start],nums[i]
+
+        K(nums,0,[],ds)
         return ds
         
